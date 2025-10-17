5fb406aa4d7bd6a40f1396b10a93617f0d75238d
SarahBoulard <sarah2boulard@gmail.com>
2025-10-17 08:58:47 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,10 @@
-function
+ function toggleCompetences() {
+      const compSection = document.getElementById('competences');
+      compSection.style.display =
+      compSection.style.display === 'none' ? 'block' : 'none';
+    }
+    function toggleFormation() {
+      const compSection = document.getElementById('Formation');
+      compSection.style.display =
+      compSection.style.display === 'none' ? 'block' : 'none';
+    }
